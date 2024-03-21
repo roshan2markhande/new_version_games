e14f2fc92618e975b3049785b12eb8a816cda133
roshan markhande <roshan.markhande@lispl.org>
2024-03-21 17:09:24 +0530

    Final

--- src/app/Components/sing-up/sing-up.component.ts
+++ src/app/Components/sing-up/sing-up.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup,Validators  } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { NgForm,NgControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sing-up',
   standalone: true,
-  imports: [RouterLink,RouterOutlet,FormsModule],
+  imports: [RouterLink,RouterOutlet,FormsModule,NgIf],
   templateUrl: './sing-up.component.html',
   styleUrl: './sing-up.component.scss'
 })
 export class SingUpComponent {
-  constructor(private http:HttpClient) {
+  email: any;
+  formSubmitted = false;
+  emailFormControl = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]);
 
+verifyPassword(){
+    this.formSubmitted = true;
+    if (this.emailFormControl.invalid) {
+      console.error('Form is invalid. Please fix validation errors.');
+      return;
+    }
+}
+
+  constructor(private http:HttpClient) {
   }
   user: any = {}; // This will store form data
-  
-  onSubmit() {
+  onSubmit() {  
+    this.verifyPassword()
   this.http.post('http://127.0.0.1:8080/register',this.user).subscribe((user:any)=>{
       console.log('User submitted:', this.user);
      });
    }
+   get emailField() {
+    return this.emailFormControl;
+  }
   } 
-
